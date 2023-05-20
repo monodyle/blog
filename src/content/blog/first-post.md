1db3907777fe1f565dc7c6fd29896eb32507b524
monodyle <monodylh@gmail.com>
2023-05-20 17:23:44 +0700

    page transition

--- src/content/blog/first-post.md
+++ src/content/blog/first-post.md
@@ -5,7 +5,11 @@ date: "Jul 08 2022"
 thumbnail: "/placeholder-hero.jpg"
 ---
 
-```sh {1,2}
+$$
+f(a, b) = \sqrt{(x_a - x_b)^2 + (y_a - y_b)^2} \lt r_a + r_b
+$$
+
+```sh prompt{1,3} {1,2}
 curl localhost:8080/actuator/health
 {"status":"UP"}
 curl localhost:8080/greeter?name=Anya
--- src/libs/blog/markdown/parse.ts
+++ src/libs/blog/markdown/parse.ts
@@ -1,19 +1,29 @@
 import { unified } from "unified";
 import remarkGfm from "remark-gfm";
+import remarkMath from "remark-math";
 import remarkParse from "remark-parse";
 import remarkRehype from "remark-rehype";
-import rehypeStringify from 'rehype-stringify'
+import rehypeStringify from "rehype-stringify";
 import rehypeStarryNight from "./code";
 import { markdownHeading } from "./heading";
+import rehypeKatex from "rehype-katex";
+import rehypeToc from "rehype-toc";
+import rehypeDocument from "rehype-document";
 
 export const parseMarkdown = async (raw: string) => {
   const compiler = unified()
     .use(remarkParse)
+    .use(remarkMath)
     .use(remarkGfm)
-    .use(remarkRehype)
+    .use(remarkRehype, { allowDangerousHtml: true })
     .use(rehypeStarryNight)
+    .use(rehypeKatex)
+    .use(rehypeDocument, {
+      css: "https://cdn.jsdelivr.net/npm/katex@0.16.0/dist/katex.min.css",
+    })
     .use(markdownHeading)
-    .use(rehypeStringify);
+    .use(rehypeToc)
+    .use(rehypeStringify, { allowDangerousHtml: true });
   const result = await compiler.process(raw);
   const content = String(result);
   return content;
