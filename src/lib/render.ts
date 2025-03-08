4f18cb45540f08765049ea73ad12919b3af9d003
monodyle <monodylh@gmail.com>
2025-03-08 19:24:46 +0700

    Render video tag in markdown

--- src/lib/render.ts
+++ src/lib/render.ts
@@ -1,27 +1,49 @@
-import rehypeCustomEmoji from 'rehype-custom-emoji'
+import rehypeShiki from '@shikijs/rehype'
+import { transformerNotationHighlight } from '@shikijs/transformers'
 import { h } from 'hastscript'
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
+import rehypeCustomEmoji from 'rehype-custom-emoji'
+import rehypeExternalLinks from 'rehype-external-links'
+import rehypeKatex from 'rehype-katex'
+import rehypeRaw from 'rehype-raw'
+import rehypeSanitize, { defaultSchema } from 'rehype-sanitize'
+import rehypeSlug from 'rehype-slug'
+import rehypeStringify from 'rehype-stringify'
+import rehypeToc from 'rehype-toc'
 import remarkGfm from 'remark-gfm'
 import remarkMath from 'remark-math'
 import remarkParse from 'remark-parse'
 import remarkRehype from 'remark-rehype'
 import { unified } from 'unified'
 import { emojis } from './emojis'
-import rehypeKatex from 'rehype-katex'
-import rehypeSlug from 'rehype-slug'
-import rehypeAutolinkHeadings from 'rehype-autolink-headings'
-import rehypeToc from 'rehype-toc'
-import rehypeStringify from 'rehype-stringify'
-import rehypeExternalLinks from 'rehype-external-links'
-import rehypeShiki from '@shikijs/rehype'
-import { transformerNotationHighlight } from '@shikijs/transformers'
 
 export function render(content: string) {
   return unified()
-    .use(remarkParse)
+    .use(remarkParse, { fragment: true })
     .use(remarkMath)
     .use(remarkGfm)
-    .use(remarkRehype)
-    .use(rehypeCustomEmoji, { emojis })
+    .use(remarkRehype, { allowDangerousHtml: true })
+    .use(rehypeRaw)
+    .use(rehypeSanitize, {
+      ...defaultSchema,
+      tagNames: [
+        ...defaultSchema.tagNames || [],
+        'video',
+        'source'
+      ],
+      attributes: {
+        ...defaultSchema.attributes,
+        video: [
+          'controls',
+          'width',
+          'height'
+        ],
+        source: [
+          'src',
+          'type'
+        ]
+      }
+    })
     .use(rehypeShiki, {
       theme: 'vitesse-light',
       colorReplacements: {
@@ -36,6 +58,7 @@ export function render(content: string) {
       content: [h('span.anchor', { ariaHidden: true })],
     })
     .use(rehypeToc)
+    .use(rehypeCustomEmoji, { emojis })
     .use(rehypeStringify, { allowDangerousHtml: true })
     .use(rehypeExternalLinks, {
       target: '_blank',
