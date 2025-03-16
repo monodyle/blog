586a219cba61b46a98518e744c75f81d8e8d1994
monodyle <monodylh@gmail.com>
2025-03-16 18:23:18 +0700

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@ import { defineConfig } from 'astro/config'
 import tailwindcss from '@tailwindcss/vite'
 import mdx from '@astrojs/mdx'
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://minhle.space',
   prefetch: true,
-  integrations: [mdx()],
+  integrations: [mdx(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
   },
