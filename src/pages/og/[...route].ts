3acbe02e2e786d37ab981041375c45e2b56f2990
monodyle <30283022+monodyle@users.noreply.github.com>
2025-11-18 16:02:00 +0700

    upgrade packages

--- src/pages/og/[...route].ts
+++ src/pages/og/[...route].ts
@@ -1,63 +1,62 @@
-import type { APIRoute } from 'astro'
+import type { APIRoute } from "astro";
 import {
   getBlogCollection,
   getNotesCollection,
   getTilCollection,
-} from 'src/lib/content'
-import { buildImage } from '../../lib/og'
+} from "src/lib/content";
+import { buildImage } from "../../lib/og";
+
+const mapper =
+  (collection: "blog" | "notes" | "til") =>
+  (entry: { slug: string | undefined }) => ({
+    params: { route: [collection, entry.slug].join("/") },
+    props: { entry },
+  });
 
 export async function getStaticPaths() {
   const [blog, notes, til] = await Promise.all([
     getBlogCollection(),
     getNotesCollection(),
     getTilCollection(),
-  ])
+  ]);
 
   const paths = [
-    ...blog.map((entry) => ({
-      params: { route: `${['blog', entry.slug].join('/')}.png` },
-      props: { entry },
-    })),
-    ...notes.map((entry) => ({
-      params: { route: `${['notes', entry.slug].join('/')}.png` },
-      props: { entry },
-    })),
-    ...til.map((entry) => ({
-      params: { route: `${['til', entry.slug].join('/')}.png` },
-      props: { entry },
-    })),
-  ]
+    ...blog.map(mapper('blog')),
+    ...notes.map(mapper('notes')),
+    ...til.map(mapper('til')),
+  ];
 
-  return paths
+  return paths;
 }
 
+const init: ResponseInit = {
+  headers: {
+    "Content-Type": "image/png",
+    "Cache-Control": "public, max-age=31536000, immutable",
+  },
+};
+
 export const GET: APIRoute = async ({ props }) => {
-  const { entry } = props
-  const title = entry?.data?.title
+  const { entry } = props;
+  const title = entry?.data?.title;
 
   if (!title) {
-    return new Response(new Uint8Array([255, 255, 255, 255]), {
-      headers: {
-        'Content-Type': 'image/png',
-        'Cache-Control': 'public, max-age=31536000, immutable',
-      },
-    })
+    return new Response(new Uint8Array([255, 255, 255, 255]), init);
   }
 
   try {
     const imageData = await buildImage({
       title,
       type: entry.collection,
-    })
+    });
 
-    return new Response(imageData, {
-      headers: {
-        'Content-Type': 'image/png',
-        'Cache-Control': 'public, max-age=31536000, immutable',
-      },
-    })
+    if (!imageData) {
+      return new Response(new Uint8Array([255, 255, 255, 255]), init);
+    }
+
+    return new Response(imageData as BodyInit, init);
   } catch (error) {
-    console.error('Error generating OG image:', error)
-    return new Response('Error generating image', { status: 500 })
+    console.error("Error generating OG image:", error);
+    return new Response("Error generating image", { status: 500 });
   }
-}
+};
